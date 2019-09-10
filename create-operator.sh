99c6b346db9ca0b761b9bbd04dad25e2cbbd8280
liuruiyiyang <245422695@qq.com>
2019-09-10 15:11:40 +0800

    change dockerhub repo name

--- create-operator.sh
+++ create-operator.sh
@@ -18,11 +18,11 @@
 set -eux;
 
 # Change the DOCKERHUB_REPO to your docker repo
-DOCKERHUB_REPO="2019liurui/rocketmq-operator:v0.0.1-snapshot"
+DOCKERHUB_REPO="docker.io/library/rocketmq-operator:v0.0.1-snapshot"
 
 export GO111MODULE=on
 
-# uncomment the following 2 lines if you have updated the [kind]_type.go file or don't have zz_generated.deepcopy.go and zz_generated.openapi.go files
+# use the following 2 commands if you have updated the [kind]_type.go file or don't have zz_generated.deepcopy.go and zz_generated.openapi.go files
 operator-sdk generate k8s
 operator-sdk generate openapi
 
