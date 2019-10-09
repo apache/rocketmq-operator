8375d2f5dec4c868cbee179539ad3fe6143d2429
vongosling <vongosling@apache.org>
2019-10-09 20:01:26 +0800

    fix(image): modify docker image repository workaroud

--- create-operator.sh
+++ create-operator.sh
@@ -18,7 +18,7 @@
 set -eux;
 
 # You can change the DOCKERHUB_REPO to your docker repo for development purpose
-DOCKERHUB_REPO="docker.io/library/rocketmq-operator:v0.0.1-snapshot"
+DOCKERHUB_REPO="rocketmqinc/rocketmq-operator:0.2.0"
 # The version of RocketMQ including the Admin Tool
 ROCKETMQ_VERSION="4.5.0"
 
