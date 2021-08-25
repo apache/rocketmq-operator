d63a4a4502513ea66b68bca79505283d74ff0543
陈少华 <chenshaohua-hj@huajiao.tv>
2021-08-25 11:30:49 +0800

    fix cpu and mem in docker for Linux

--- images/namesrv/alpine/runserver-customize.sh
+++ images/namesrv/alpine/runserver-customize.sh
@@ -54,8 +54,8 @@ calculate_heap_sizes()
 {
     case "`uname`" in
         Linux)
-            system_memory_in_mb=`free -m| sed -n '2p' | awk '{print $2}'`
-            system_cpu_cores=`egrep -c 'processor([[:space:]]+):.*' /proc/cpuinfo`
+            system_memory_in_mb=$(($(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)/1024/1024))
+            system_cpu_cores=$(($(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)/100000))
         ;;
         FreeBSD)
             system_memory_in_bytes=`sysctl hw.physmem | awk '{print $2}'`
